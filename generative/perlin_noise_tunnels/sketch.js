029d2cbb02326751eadc2f1bb3d1f5fe172711a7
Luca Ionescu <lucaionescu@users.noreply.github.com>
2019-11-09 22:00:23 +0100

    add generative site.

---
+++ generative/perlin_noise_tunnels/sketch.js
@@ -0,0 +1,53 @@
+let n = 1000;
+let t = 0;
+let particles = [];
+let name = "perlin_noise_tunnels";
+
+function Particle() {
+  this.loc = createVector(random(width), random(height));
+  this.vel = createVector(0, 0);
+  this.i = random(-10, 10);
+
+  this.display = function () {
+    point(this.loc.x, this.loc.y);
+  }
+
+  this.update = function (t) {
+    this.loc.x += this.vel.x;
+    this.loc.y += this.vel.y;
+    this.vel = p5.Vector.fromAngle((noise(t, this.i) * TWO_PI) * 2);
+    this.vel.add(p5.Vector.fromAngle((noise(this.loc.x / 100, this.loc.y / 100) * TWO_PI) * 3));
+    this.i += 0.01;
+  }
+}
+
+function setup() {
+  pixelDensity(3);
+
+  createCanvas(windowWidth, windowHeight);
+  stroke(255, 10);
+  background("#19181a");
+
+  for (let i = 0; i < n; i++) {
+    particles[i] = new Particle();
+  }
+}
+
+function draw() {
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update(t);
+    particles[i].display();
+  }
+
+  t += 0.01;
+}
+
+function keyPressed() {
+  if (keyCode == 83) {
+    var today = new Date();
+    var date = today.getFullYear() + "_" + (today.getMonth() + 1) + "_" + today.getDate();
+    var time = today.getHours() + "_" + today.getMinutes() + "_" + today.getSeconds();
+    var fileName = name + "_" + date + "_" + time;
+    saveCanvas(fileName, "png")
+  }
+}
